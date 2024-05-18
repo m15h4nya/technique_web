5a8f5f96a81a75366c1ab900425fed7100edec44
Alsu <rakhmanova_2003@inbox.ru>
2024-05-18 19:15:49 +0300

    fetch + show example

--- main.js
+++ main.js
@@ -14,3 +14,41 @@ document.addEventListener('DOMContentLoaded', function() {
     loginPanel.classList.toggle('hidden');
   });
 });
+
+
+let block = document.querySelector('.main_content-list')
+
+const host = 'http://localhost:8081';
+const web_host = 'http://localhost:8080';
+const all_files = '/api/all_files'; 
+
+fetch(host+all_files)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => { // [string]
+    // Display data in an HTML element
+    data.forEach(element => {
+      printLi(element)  
+    });
+
+
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+  
+
+let printLi = (elem) => {
+  let li = document.createElement('li');
+  let a = document.createElement('a');
+  a.textContent = elem; // elem = blabla
+  a.classList.add('main_a')
+  a.href = web_host + "/" + elem;
+  li.appendChild(a);
+  // li.textContent = blabla
+  block.appendChild(li);
+}
