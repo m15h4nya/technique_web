5da22ba5d759696cde819b705b6c83f982d2ce19
Alsu <rakhmanova_2003@inbox.ru>
2024-05-28 19:49:18 +0300

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import MainContent from './ContentMain/ContentMain.jsx';
 import Header from './Header/Header.jsx';
 import Aside from './Aside/Aside.jsx';
-import HeaderName from './Header/HeaderName.jsx';
 import './App.scss';
 import { useState } from 'react';
 
